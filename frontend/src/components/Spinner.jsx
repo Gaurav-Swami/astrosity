a7122b9c96b68673f6d5e195baee323ec062e594
Gaurav-Swami <gauravswami0404@gmail.com>
2024-09-07 17:40:41 +0530

    commit

--- frontend/src/components/Spinner.jsx
+++ frontend/src/components/Spinner.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Spinner = () => {
-  return (
+  return (//test
     <div className="flex items-center justify-center h-screen w-full overflow-x-clip">
       <div className="w-16 h-16    shadow-accent border-4 border-t-4 dark:border-primaryBg border-lightPrimaryBg border-t-accent dark:border-t-accent  rounded-full animate-spin "></div>
     </div>
